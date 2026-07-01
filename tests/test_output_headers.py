a4b48d519a89b288d7bf04c0ca03b06c68b72b04
Jens Luebeck <jluebeck@ucsd.edu>
2026-07-01 10:48:42 -0700

    test if it can take graph with header as input

--- tests/test_output_headers.py
+++ tests/test_output_headers.py
@@ -31,6 +31,25 @@ class OutputHeaderTests(unittest.TestCase):
             self.assertEqual(svs, [])
             self.assertEqual(len(chrom_segs["chr1"]), 2)
 
+    def test_graph_parser_accepts_comment_header(self):
+        graph_text = "\n".join([
+            "#AmpliconArchitect 1.4.0",
+            "SequenceEdge: StartPosition, EndPosition, PredictedCN, AverageCoverage, Size, NumberReadsMapped",
+            "sequence\tchr1:100-\tchr1:199+\t4\t12.5\t100\t7",
+            "BreakpointEdge: StartPosition->EndPosition, PredictedCN, NumberOfReadPairs",
+            "discordant\tchr1:100-->chr1:100-\t2\t6",
+            "",
+        ])
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            graph_path = Path(tmpdir) / "headered_graph.txt"
+            graph_path.write_text(graph_text)
+
+            svs, chrom_segs = parse_graph_file(graph_path)
+
+            self.assertEqual(len(svs), 1)
+            self.assertEqual(len(chrom_segs["chr1"]), 1)
+
     def test_cycles_writer_emits_version_header_and_remains_parseable(self):
         segments = [("chr1", 100, 199, 4, 12.5, 7)]
 
